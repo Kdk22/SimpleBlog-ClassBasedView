1a9777295b42df63f984b6a588c22dab6e792975
Kdk22 <khadkautshav7@gmail.com>
2019-05-01 14:09:18 +0545

    COmment form

--- Blog/views.py
+++ Blog/views.py
@@ -236,6 +236,19 @@ class PostComment(CreateView):
     # template_name = 'Blog/details.html'
     success_url = reverse_lazy('Blog:index')
 
+    def get_success_url(self):
+        """Return the URL to redirect to after processing a valid form."""
+        if self.success_url:
+            url = self.success_url.format(**self.object.__dict__)
+        else:
+            try:
+                url = self.object.get_absolute_url()
+            except AttributeError:
+                raise ImproperlyConfigured(
+                    "No URL to redirect to.  Either provide a url or define"
+                    " a get_absolute_url method on the Model.")
+        return url
+
     def post(self, request, *args, **kwargs):
         """
         Handle POST requests: instantiate a form instance with the passed
